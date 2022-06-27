b251072c6d2244b2c7c1190505a00f24efa6521d
Tamil8554 <tamilraja999@gmail.com>
2022-06-27 16:23:48 +0530

    tamil

---
+++ Arm.java
@@ -0,0 +1,17 @@
+
+public class Arm{
+
+public static void main(String[] args){
+
+System.out.println("one");
+System.out.println("two");
+System.out.println("thre");
+System.out.println("four");
+
+
+
+}
+
+
+
+}
